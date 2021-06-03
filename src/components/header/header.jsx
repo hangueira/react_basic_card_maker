d22bf534722bea31af384867f3d9903762f1c4de
hangueira <hks713@naver.com>
2021-06-03 16:55:36 +0900

    add login and logout

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,7 +8,7 @@ const Header = ({ onLogout }) => (
         Logout
       </button>
     )}
-    <img className={styles.logo} src="/images/logo.png" alt="logo" />
+    <img className={styles.logo} src='/images/logo.png' alt='logo' />
     <h1 className={styles.title}>Business Card Maker</h1>
   </header>
 );
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,11 +1,21 @@
-import firebase from "@firebase/app";
-import firebaseApp from "./firebase";
-import "@firebase/auth";
+import firebase from '@firebase/app';
+import firebaseApp from './firebase';
+import '@firebase/auth';
 
 class AuthService {
   login(providerName) {
     const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
     return firebaseApp.auth().signInWithPopup(authProvider);
+  }
+
+  logout() {
+    firebase.auth().signOut();
+  }
+
+  onAuthChange(onUserChanged) {
+    firebase.auth().onAuthStateChanged((user) => {
+      onUserChanged(user);
+    });
   }
 }
 
